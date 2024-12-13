5de951f1e30e970ce04519fc098455c7bc9b0844
SolntsevaD <solnsevadacha@gmail.com>
2024-12-13 10:41:44 +0300

    scaled render

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,23 @@
 let WIDTH = 800;
 let HEIGHT = 600;
 let SCALE = 4;
-let d = new Date();
 let previousTime = 0;
-let currentTime = 0;
 let passedTime = 0;
-let msPerFrame = 1000.0 / 144.0;
-let imageData;
+let msPerFrame = 1000.0 / 60.0;
+let screen;
 let cvs;
 let gfx;
-function start()
-{
+
+let frameTime;
+let a = 255;
+class Bitmap {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+        this.pixels = new Uint32Array(width * height);
+    }
+}
+function start() {
     init();
     run();
 }
@@ -20,35 +27,62 @@ function init()
     cvs.setAttribute("width", WIDTH + "px");
     cvs.setAttribute("height", HEIGHT + "px");
     gfx = cvs.getContext("2d");
+    cvs.onfocus = function() {
+        console.log(1);
+    }
+    cvs.focus();
+    frameTime = document.getElementById("frame");
     WIDTH = WIDTH / SCALE;
     HEIGHT = HEIGHT / SCALE;
-    imageData = new ImageData(WIDTH, HEIGHT);
     previousTime = new Date().getTime();
+    screen = new Bitmap(WIDTH, HEIGHT);
+    for (let i = 0; i < WIDTH * HEIGHT; i++) {
+        screen.pixels[i] = Math.random() * 0xffffff;
+    }
 }
-function run(time)
-{
+function run(time) {
     let currentTime = new Date().getTime();
     passedTime += currentTime - previousTime;
     previousTime = currentTime;
-    while (passedTime >= msPerFrame)
-    {
-        update(msPerFrame);
+    while (passedTime >= msPerFrame) {
+        update(passedTime);
         render();
         passedTime -= msPerFrame;
     }
     requestAnimationFrame(run);
 }
-function update(msPerFrame)
-{
-    for (let index = 0; index < imageData.data.length; index++)
-    {
-        imageData.data[index] = Math.random() * 255;
+function update(delta) {
+    for (let i = 0; index < screen.pixels.length; i++) {
+        screen.pixels[i] = Math.random() * 0xffffff;
     }
+    frameTime.innerHTML = Math.floor(1000.0 / delta) + "fps";
 }
-function render()
-{
-    
-    gfx.putImageData(imageData, 0, 0);
+function render() {  
+    gfx.putImageData(convert(screen, SCALE), 0, 0);
 }
-
+function convert(bitmap, scale){
+    let res = new ImageData(bitmap.width * scale, bitmap.height * scale);
+    for (let y = 0; y < bitmap.height; y++)
+    {
+        for (let x = 0; x < bitmap.width; x++)
+        {
+            let bitmapPixel = bitmap.pixels[x + y * bitmap.width]
+            let r = (bitmapPixel >> 16) & 0xff;
+            let g = (bitmapPixel >> 8) & 0xff;
+            let b = bitmapPixel & 0xff;
+            for (let ys = 0; ys < SCALE; ys++)
+            {
+                for (let xs = 0; xs < SCALE; xs++)
+                {
+                    let ptr = ((x * SCALE) + xs + ((y * SCALE) + ys) * res.width) * 4;
+                    res.data[ptr] = r;
+                    res.data[ptr + 1] = g;
+                    res.data[ptr + 2] = b;
+                    res.data[ptr + 3] = a;
+                }
+            }
+        }
+    }
+    return res;
+}
 window.onload = start;
